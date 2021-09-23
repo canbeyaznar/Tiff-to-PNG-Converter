d37a522b84726ca72ca186f6640c0621902e0684
Can Beyaznar <32619606+canbeyaznar@users.noreply.github.com>
2021-09-23 18:02:26 +0300

    Add files via upload

---
+++ tiff_to_png_format.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageEnhance
+
+def tiff_to_png( src, dst ):
+    im = Image.open(src)
+    str = dst.__str__() +".png"
+    im.mode = 'I'
+    im.point(lambda i:i*(1./1)).convert('L').save(str)
+
+def main():
+    tiff_to_png("1.tiff", "1")
+
+if __name__ == "__main__":
+    main()
